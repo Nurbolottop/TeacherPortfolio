d2a369681993942361941b332158b19e1bba6e70
Nurbolottop <erk1nbaevw2711@gmail.com>
2024-02-20 12:34:22 +0600

    dsf

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Или любой другой путь, где вы хотите собирать статические файлы
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Или любой другой путь, где вы хотите собирать статические файлы
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Если у вас есть дополнительные статические файлы для вашего приложения
 ]
@@ -141,45 +141,45 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # JAZZMIN
-JAZZMIN_SETTINGS = {
-    "site_title": "Visitka.KG",  # Заголовок админ-панели
-    "site_header": "Visitka.KG",  # Заголовок на экране входа
-    "site_brand": "Visitka.KG",  # Бренд в верхней части админ-панели
-    "welcome_sign": "Добро пожаловать в Visitka.KG",  # Приветственное сообщение
-    "site_title": "Vizitka.KG",  # Заголовок админ-панели
-    "site_header": "Vizitka.KG",  # Заголовок на экране входа
-    "site_brand": "Vizitka.KG",  # Бренд в верхней части админ-панели
-    "welcome_sign": "Добро пожаловать в Vizitka.KG",  # Приветственное сообщение
-    "search_model": ["auth.User", "blog.Post"],  # Модели, доступные для поиска
-    "topmenu_links": [
-        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
-    ],
-    "show_sidebar": True,
-    "changeform_format": "horizontal_tabs",
-    "header_classes": "navbar-dark bg-dark",  # Темный фон верхней части админ-панели
-    "header_color": "#000000",  # Черный цвет верхней части админ-панели
-    "dark_mode_theme": True,  # Включить темный режим
-    "show_language_chooser": True,  # Включить выбор языка в админ-панели
-    "custom_css": None,  # Путь к пользовательскому CSS-файлу (если нужен)
-    "show_ui_builder": True,  # Показать UI Builder
-    "menu": [
-        {
-            "app": "index",  # Имя вашего приложения Django
-            "name": "Основные параметры",  # Имя модели
-            "icon": "fa fa-cogs",  # Иконка для меню
-            "models": [
-                {
-                    "name": "Первая модель",  # Имя вашей модели
-                    "icon": "fa fa-cog",  # Иконка для модели
-                    "model": "index.Settings",  # Имя модели в формате "app_label.model_name"
-                },
-                # Добавьте другие модели, если необходимо
-            ],
-        },
-        # Добавьте другие приложения и модели, если необходимо
-    ],
+# JAZZMIN_SETTINGS = {
+#     "site_title": "Visitka.KG",  # Заголовок админ-панели
+#     "site_header": "Visitka.KG",  # Заголовок на экране входа
+#     "site_brand": "Visitka.KG",  # Бренд в верхней части админ-панели
+#     "welcome_sign": "Добро пожаловать в Visitka.KG",  # Приветственное сообщение
+#     "site_title": "Vizitka.KG",  # Заголовок админ-панели
+#     "site_header": "Vizitka.KG",  # Заголовок на экране входа
+#     "site_brand": "Vizitka.KG",  # Бренд в верхней части админ-панели
+#     "welcome_sign": "Добро пожаловать в Vizitka.KG",  # Приветственное сообщение
+#     "search_model": ["auth.User", "blog.Post"],  # Модели, доступные для поиска
+#     "topmenu_links": [
+#         {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+#     ],
+#     "show_sidebar": True,
+#     "changeform_format": "horizontal_tabs",
+#     "header_classes": "navbar-dark bg-dark",  # Темный фон верхней части админ-панели
+#     "header_color": "#000000",  # Черный цвет верхней части админ-панели
+#     "dark_mode_theme": True,  # Включить темный режим
+#     "show_language_chooser": True,  # Включить выбор языка в админ-панели
+#     "custom_css": None,  # Путь к пользовательскому CSS-файлу (если нужен)
+#     "show_ui_builder": True,  # Показать UI Builder
+#     "menu": [
+#         {
+#             "app": "index",  # Имя вашего приложения Django
+#             "name": "Основные параметры",  # Имя модели
+#             "icon": "fa fa-cogs",  # Иконка для меню
+#             "models": [
+#                 {
+#                     "name": "Первая модель",  # Имя вашей модели
+#                     "icon": "fa fa-cog",  # Иконка для модели
+#                     "model": "index.Settings",  # Имя модели в формате "app_label.model_name"
+#                 },
+#                 # Добавьте другие модели, если необходимо
+#             ],
+#         },
+#         # Добавьте другие приложения и модели, если необходимо
+#     ],
 
-}
+# }
 
 
 
